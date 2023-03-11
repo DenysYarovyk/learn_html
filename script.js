fb5ce56170a492afe8ce5d6d700eaea3ef046e74
Denys Yarovyk <denisarovik@gmail.com>
2023-03-11 11:40:46 +0200

    all chanched for March 11 at 11:40

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-alert('Hello world!')
+// alert('Hello world!')
+
+let myArr = ['Audi', 'BMW', 'Ford', 'Toyota'];
+
+    function showContent() {
+      let temp, item, a, i;
+      temp = document.getElementsByTagName('template')[0];
+      item = temp.content.querySelector('div');
+      for (i = 0; i < myArr.length; i++) {
+        a = document.importNode(item, true);
+        a.textContent += myArr[i];
+        document.body.appendChild(a);
+      };
+    };
+
+function showDetails(animal) {
+  let animalType = animal.getAttribute("data-animal-type");
+  alert("The " + animal.innerHTML + " is a " + animalType + ".");
+}
+
+const turnOnBtn = document.getElementById("turnOnBtn");
+const turnOffBtn = document.getElementById("turnOffBtn");
+const myImage = document.getElementById("myImage");
+
+turnOnBtn.addEventListener("click", function() {
+  myImage.src = "/img/pic_bulbon.gif";
+});
+
+turnOffBtn.addEventListener("click", function() {
+  myImage.src = "/img/pic_bulboff.gif";
+});
